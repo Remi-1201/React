392aed44d3e1c39543a919d37acf23c3db9d1f67
Remi <97021497+Remi-1201@users.noreply.github.com>
2022-06-09 18:09:34 +0900

    タスクの完了機能

--- src/index.js
+++ src/index.js
@@ -17,7 +17,33 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "Finished";
   completeButton.addEventListener("click", () => {
-    alert("Delete");
+    // delete div "list-row" attribute when button clicked
+    const deleteTarget = completeButton.parentNode;
+    deleteFromIncompleList(completeButton.parentNode);
+
+    // 完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+
+    // TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    // div以下を初期化
+    addTarget.textContent = null;
+
+    // liタグを生成
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    // Create button tags
+    const backButton = document.createElement("button");
+    backButton.innerText = "Move back";
+
+    // divタグの子要素に各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    // Add to Finished-list
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   // button(delete)タグ生成
@@ -25,8 +51,7 @@ const onClickAdd = () => {
   deleteButton.innerText = "Delete";
   deleteButton.addEventListener("click", () => {
     // delete div "list-row" attribute when button clicked
-    const deleteTarget = deleteButton.parentNode;
-    document.getElementById("imcomplete-list").removeChild(deleteTarget);
+    deleteFromIncompleList(deleteButton.parentNode);
   });
 
   // divタグの子要素に各要素を設定
@@ -36,6 +61,11 @@ const onClickAdd = () => {
 
   // 未完了のリストに追加
   document.getElementById("imcomplete-list").appendChild(div);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleList = (target) => {
+  document.getElementById("imcomplete-list").removeChild(target);
 };
 
 document
